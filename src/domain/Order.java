6ad9a5ab9d2020b81d05568e467737b93ac65380
teixeirajr09 <danilotpjr@gmail.com>
2025-12-24 01:03:37 -0300

    Introduce Order entity with CREATED initial status

--- src/domain/Order.java
+++ src/domain/Order.java
@@ -1,5 +1,28 @@
 package domain;
 
-public class Order {
+import java.util.UUID;
 
+public class Order {
+    private final UUID id;
+    private final Priority priority;
+    private OrderStatus status;
+
+
+    public Order(Priority priority) {
+        this.id = UUID.randomUUID();
+        this.priority = priority;
+        this.status = OrderStatus.CREATED;
+    }
+
+    public UUID getID() {
+        return id;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
 }
